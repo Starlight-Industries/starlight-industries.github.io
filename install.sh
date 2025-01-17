e1522ed0d61cde5fd3b78566d97adace4ea3b52a
Caznix <Caznix01@gmail.com>
2025-01-17 18:00:30 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+echo "This is a test. But thanks for Trying!"
+echo "See the following link for more info:"
+echo "https://github.com/Starlight-Industries/Comet"
+echo "If you have any questions, chat with us on Discord: https://discord.com/invite/xJX4GXvbME"
